e292f0da7e602d3fdb1c41a6efc4ed28eaa46f47
Saber1013 <2460908022@qq.com>
2026-06-29 17:43:25 +0800

    chore: remove unused frontend files

--- frontend-app/src/features/face-swap/utils.js
+++
@@ -1,9 +0,0 @@
-export {
-  formatBytes,
-  normalizeSourceDuration,
-  cleanDisplayName,
-  readVideoFileDuration,
-  PROCESSING_STEPS,
-  getStepState,
-  deriveWorkflowStatus,
-} from "../video-workflow/utils";
