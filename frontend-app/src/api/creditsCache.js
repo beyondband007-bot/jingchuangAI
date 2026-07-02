abe9d67585e22ac0134c48df935e4af07f7bbbfa
Saber1013 <2460908022@qq.com>
2026-07-02 14:33:16 +0800

    fix: guard credits and article generation submissions

--- frontend-app/src/api/creditsCache.js
+++ frontend-app/src/api/creditsCache.js
@@ -1,21 +1,38 @@
 import { requestJson as request } from "./request.js";
 
 let creditsPromise;
+let latestCreditsPromise;
+let creditsRequestVersion = 0;
+
+function requestCredits() {
+  const requestVersion = ++creditsRequestVersion;
+  const pendingRequest = request("/api/me/credits");
+  const guardedRequest = pendingRequest
+    .then((credits) => {
+      if (requestVersion !== creditsRequestVersion) {
+        return latestCreditsPromise;
+      }
+      return credits;
+    })
+    .catch((err) => {
+      if (requestVersion === creditsRequestVersion) {
+        creditsPromise = undefined;
+      }
+      return Promise.reject(err);
+    });
+
+  latestCreditsPromise = guardedRequest;
+  creditsPromise = guardedRequest;
+  return guardedRequest;
+}
 
 export function getCachedCredits() {
   if (!creditsPromise) {
-    creditsPromise = request("/api/me/credits").catch((err) => {
-      creditsPromise = undefined;
-      return Promise.reject(err);
-    });
+    return requestCredits();
   }
   return creditsPromise;
 }
 
 export function refreshCachedCredits() {
-  creditsPromise = request("/api/me/credits").catch((err) => {
-    creditsPromise = undefined;
-    return Promise.reject(err);
-  });
-  return creditsPromise;
+  return requestCredits();
 }
